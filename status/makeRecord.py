df259f4ec072f7b2a4f12647f2d191ebcdf9982d
EmilB <emilBcn33@gmail.com>
2020-10-14 00:00:12 +0200

    status history

---
+++ status/makeRecord.py
@@ -0,0 +1,33 @@
+import json
+import sys
+import re
+
+def makeRecord(filename):
+	record = ''
+	file = open(filename)
+	data = json.load(file)
+	states = data['MachineStates']
+	for state in states:
+		text = state['Text1']
+		isWasher = state['GroupNumber'] == 0
+		if text == 'Idle':
+			record += ' I' if isWasher else ' I 0'
+			continue
+		elif text == 'Bought':
+			record += ' B' if isWasher else ' B 0'
+			continue
+		elif text == 'Busy':
+			record += ' Y' if isWasher else ' Y 0'
+			continue
+
+		record += ' ' + re.search('([0-9]*)(?= min)', text).group(0)
+		if not isWasher: # Dryer
+			humitidy = re.search('(<=RH: )([0-9]*)', text)
+			if humitidy:
+				record += ' ' + humitidy.group(0)
+	file.close()
+	return record
+
+path1 = sys.argv[1]
+path2 = sys.argv[2]
+print(makeRecord(path1), makeRecord(path2), sep='')
--- status/status.sh
+++ status/status.sh
@@ -8,10 +8,26 @@ username=$1
 password=$2
 
 token=$(curl --data "scope=DA" --data "client_id=YV1ZAQ7BTE9IT2ZBZXLJ" --data "username="$username --data "password="$password --data "grant_type=password" -X POST https://sec.mielelogic.com/v3/token | jq -r '.access_token')
-echo $token
+# echo $token
 
 curl -H "Authorization: Bearer "$token https://api.mielelogic.com/v3/Country/DA/Laundry/9894/laundrystates?language=en > $local_path/status1.json
 curl -H "Authorization: Bearer "$token https://api.mielelogic.com/v3/Country/DA/Laundry/9893/laundrystates?language=en > $local_path/status2.json
 
-cat $local_path/status1.json
-cat $local_path/status2.json
+# cat $local_path/status1.json
+# cat $local_path/status2.json
+
+# Append to status history
+timestamp=$(date +'%Y-%m-%d %T')
+last_record=$(tail -n 1 history.txt | cut -c 20- || echo '')
+
+record=$(python $local_path/makeRecord.py $local_path/status1.json $local_path/status2.json)
+echo $last_record
+echo $record
+
+# Don't rocord if nothing has changed
+if [[ "$last_record" == "$record" ]]; then
+	echo exit
+	exit
+fi
+
+echo $timestamp$record >> history.txt
